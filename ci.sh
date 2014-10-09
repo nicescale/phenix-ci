436a95138f814e775cf341aeb2f45aff6dd5d3a4
William Hanwoody <hanwoody@gmail.com>
2014-10-09 18:34:53 +0800

    output realtime

--- ci.sh
+++ ci.sh
@@ -12,13 +12,6 @@ fi
 
 IP=$(/sbin/ifconfig|grep -A 4 -P '^eth'| grep 'inet '|awk -F: '{print $2}'|awk '{print $1}'|head -n 1)
 
-docker run -it -d --name=$TEST_CONTAINER -e KEY_SERVER_URL=http://$IP:9998 -e JOB_WORKER_URL=http://$IP:5919 -e DB_NAME=phenix_test --link=${MONGODB_CONTAINER}:mongodb -v $WORKSPACE/case.json:/go/src/github.com/mountkin/phenix-api/api/test/case.json $BUILDER_IMAGE /go/src/github.com/mountkin/phenix-api/api/test/run.sh
-
-docker wait $TEST_CONTAINER
-echo "======================================================"
-echo "                 FINAL TEST OUTPUT"
-echo "======================================================"
-docker logs $TEST_CONTAINER | tee /tmp/ci1118.log
-echo "======================================================"
+docker run -it --rm --name=$TEST_CONTAINER -e KEY_SERVER_URL=http://$IP:9998 -e JOB_WORKER_URL=http://$IP:5919 -e DB_NAME=phenix_test --link=${MONGODB_CONTAINER}:mongodb -v $WORKSPACE/case.json:/go/src/github.com/mountkin/phenix-api/api/test/case.json $BUILDER_IMAGE /go/src/github.com/mountkin/phenix-api/api/test/run.sh | tee /tmp/ci1118.log
 
 cat /tmp/ci1118.log | grep -q PASS
