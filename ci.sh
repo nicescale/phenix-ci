4e861095deb769bd9ccc11fdd35498d62bcea8b7
William Hanwoody <hanwoody@gmail.com>
2014-10-09 17:50:03 +0800

    first commit

---
+++ ci.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+BUILDER_IMAGE="gobuilder"
+TEST_CONTAINER="gotest1"
+MONGODB_CONTAINER="phenix-mongodb"
+
+docker rm -f $TEST_CONTAINER
+docker run -it -d --name=$TEST_CONTAINER -e KEY_SERVER_URL=http://127.0.0.1:9998 -e JOB_WORKER_URL=http://127.0.0.1:5919 -e DB_NAME=phenix_test --link=${MONGODB_CONTAINER}:mongodb -v $WORKSPACE/case.json:/go/src/github.com/mountkin/phenix-api/api/test/case.json $BUILDER_IMAGE /go/src/github.com/mountkin/phenix-api/api/test/run.sh
+
+docker wait $TEST_CONTAINER
+echo "======================================================"
+echo "                 FINAL TEST OUTPUT"
+echo "======================================================"
+docker logs $TEST_CONTAINER | tee /tmp/ci1118.log
+echo "======================================================"
+
+cat /tmp/ci1118.log | grep -q PASS
