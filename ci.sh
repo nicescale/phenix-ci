e6d013d71e67f852c22f5c255c80e84b6c4eda82
William Hanwoody <hanwoody@gmail.com>
2014-10-10 18:39:43 +0800

    fix output

--- ci.sh
+++ ci.sh
@@ -12,6 +12,8 @@ fi
 
 IP=$(/sbin/ifconfig|grep -A 4 -P '^eth'| grep 'inet '|awk -F: '{print $2}'|awk '{print $1}'|head -n 1)
 
-docker run -it --rm --name=$TEST_CONTAINER -e KEY_SERVER_URL=http://$IP:9998 -e JOB_WORKER_URL=http://$IP:5919 -e DB_NAME=phenix_test --link=${MONGODB_CONTAINER}:mongodb -v $WORKSPACE/case.json:/go/src/github.com/mountkin/phenix-api/api/test/case.json $BUILDER_IMAGE /go/src/github.com/mountkin/phenix-api/api/test/run.sh | tee /tmp/ci1118.log
+OUTPUT=$(docker run -it --rm --name=$TEST_CONTAINER -e KEY_SERVER_URL=http://$IP:9998 -e JOB_WORKER_URL=http://$IP:5919 -e DB_NAME=phenix_test --link=${MONGODB_CONTAINER}:mongodb -v $WORKSPACE/case.json:/go/src/github.com/mountkin/phenix-api/api/test/case.json $BUILDER_IMAGE /go/src/github.com/mountkin/phenix-api/api/test/run.sh)
 
-cat /tmp/ci1118.log | grep -q PASS
+cat $OUTPUT
+
+cat $OUTPUT | grep -q PASS
