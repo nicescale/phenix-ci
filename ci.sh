63429ce32e3ac38b99d7d648bd38412ed661fec0
Shijiang Wei <mountkin@gmail.com>
2014-12-18 15:45:56 +0800

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -13,7 +13,7 @@ fi
 
 IP=$(/sbin/ifconfig|grep -A 4 -P '^eth'| grep 'inet '|awk -F: '{print $2}'|awk '{print $1}'|head -n 1)
 
-OUTPUT=$(docker run --rm --name=$TEST_CONTAINER -e REDIS_URL=$IP:9996 -e KEY_SERVER_URL=http://$IP:9998 -e JOB_WORKER_URL=http://$IP:5919 -e DB_NAME=phenix_test --link=${MONGODB_CONTAINER}:mongodb -v $WORKSPACE/case.json:/go/src/github.com/nicescale/phenix-api/api/test/case.json $BUILDER_IMAGE /go/src/github.com/nicescale/phenix-api/api/test/run.sh 2>&1)
+OUTPUT=$(docker run --rm --name=$TEST_CONTAINER -e REDIS_URL=$IP:9996 -e KEY_SERVER_URL=http://$IP:9998 -e JOB_WORKER_URL=http://$IP:9990 -e DB_NAME=phenix_test --link=${MONGODB_CONTAINER}:mongodb -v $WORKSPACE/case.json:/go/src/github.com/nicescale/phenix-api/api/test/case.json $BUILDER_IMAGE /go/src/github.com/nicescale/phenix-api/api/test/run.sh 2>&1)
 
 echo $OUTPUT
 
